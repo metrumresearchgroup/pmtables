267ba95a1bf54ddcab1238be7e373c7851f5fec7
Kyle Baron <kyleb@metrumrg.com>
2020-06-01 10:07:45 -0500

    re-factor data inventory functions to by / panel syntax

--- R/data_inventory_table.R
+++ R/data_inventory_table.R
@@ -7,12 +7,12 @@
 #' @param bq_col character name of `BQL` column
 #' @param id_col character name of `ID` column
 #' @param ... used to absorb other arguments; not used
-data_inventory_chunk <- function(data, outer, inner = outer, stacked = FALSE,
+data_inventory_chunk <- function(data, by, panel = by, stacked = FALSE,
                                  tot = FALSE, all_name = "all",
                                  dv_col = "DV", bq_col = "BQL", id_col = "ID",
                                  ...) {
 
-  if(outer==".total" | inner==".total") {
+  if(by==".total" | panel == ".total") {
     data <- data_total_col(data,all_name = all_name)
   }
 
@@ -27,14 +27,17 @@ data_inventory_chunk <- function(data, outer, inner = outer, stacked = FALSE,
     )
     glue::glue(paste0(x,collapse=""))
   }
+
   if(!exists(dv_col,data)) {
     emessage(miss_required("DV", "dv_col"))
     miss <- TRUE
   }
+
   if(!exists(bq_col,data)) {
     emessage(miss_required("BQL", "bq_col"))
     miss <- TRUE
   }
+
   if(!exists(id_col,data)) {
     emessage(miss_required("ID", "id_col"))
     miss <- TRUE
@@ -50,21 +53,21 @@ data_inventory_chunk <- function(data, outer, inner = outer, stacked = FALSE,
     )
   }
 
-  by <- unique(c(outer,inner))
+  .groups <- unique(c(panel,by))
 
   data <- ungroup(data)
 
   if(stacked) {
-    data <- group_by(data, !!sym(outer))
+    data <- group_by(data, !!sym(panel))
     data <- mutate(data,.N = n_non_missing(!!sym(dv_col)))
     data <- ungroup(data)
   } else {
     data <- mutate(data,.N = n_non_missing(!!sym(dv_col)))
   }
-  data <- group_by(data, !!sym(outer))
+  data <- group_by(data, !!sym(by))
   data <- mutate(data,..n = n_non_missing(!!sym(dv_col)))
   data <- ungroup(data)
-  data <- group_by(data, !!!syms(by))
+  data <- group_by(data, !!!syms(.groups))
   body <- summarise(
     data,
     SUBJ = n_unique(!!sym(id_col)),
@@ -79,11 +82,11 @@ data_inventory_chunk <- function(data, outer, inner = outer, stacked = FALSE,
     PBQL = digit1(100*.data[["NBQL"]]/first(.data[["..n"]])),
     OBQL = digit1(100*.data[["NBQL"]]/first(.data[[".N"]]))
   )
-  summ <- left_join(body,bq,by = unique(c(outer,inner)))
+  summ <- left_join(body,bq,by = unique(c(by,panel)))
   summ <- select(
     summ,
-    !!sym(outer),
-    !!sym(inner),
+    !!sym(by),
+    !!sym(panel),
     .data[["SUBJ"]],
     .data[["NMISS"]],
     .data[["NOBS"]],
@@ -94,17 +97,17 @@ data_inventory_chunk <- function(data, outer, inner = outer, stacked = FALSE,
     .data[["OBQL"]]
   )
   summ <- ungroup(summ)
-  summ <- mutate(summ, !!sym(outer) := as.character(!!sym(outer)))
-  summ <- mutate(summ, !!sym(inner) := as.character(!!sym(inner)))
+  summ <- mutate(summ, !!sym(by) := as.character(!!sym(by)))
+  summ <- mutate(summ, !!sym(panel) := as.character(!!sym(panel)))
   summ
 }
 
-data_inventory_data_split <- function(data,outer,inner=outer,stacked=FALSE,...) {
+data_inventory_data_split <- function(data,by,panel=by,stacked=FALSE,...) {
   data <- ungroup(data)
-  data <- split(data,data[[outer]],drop=TRUE)
-  data <- map_dfr(data,data_inventory_data,outer=outer,inner=inner,stacked=FALSE,...)
-  data <- mutate(data, !!sym(inner) := replace_na(!!sym(inner),"all"))
-  data <- fill(data,!!sym(outer),.direction = "down")
+  data <- split(data,data[[panel]],drop=TRUE)
+  data <- map_dfr(data,data_inventory_data,by=by,panel=panel,stacked=FALSE,...)
+  data <- mutate(data, !!sym(by) := replace_na(!!sym(by),"all"))
+  data <- fill(data,!!sym(panel),.direction = "down")
   data[[".total"]] <- NULL
   data
 
@@ -117,44 +120,45 @@ data_inventory_data_split <- function(data,outer,inner=outer,stacked=FALSE,...) 
 #' @param ... passed to subsequent summary functions
 #'
 #' @export
-data_inventory_data <- function(data, outer,inner=outer,all_name = "all",
-                                stacked=FALSE, ...) {
-  outer <- unname(outer)
-  inner <- unname(inner)
+data_inventory_data <- function(data, by, panel = by, all_name = "all",
+                                stacked = FALSE, ...) {
+  by <- unname(by)
+  panel <- unname(panel)
 
   if(stacked) {
-    ans <- data_inventory_data_split(data,outer,inner, stacked = FALSE, ...)
+    ans <- data_inventory_data_split(data, by, panel, stacked = FALSE, ...)
     return(ans)
   }
 
   data <- data_total_col(data, all_name = all_name)
-  check_discrete(data,cols = unique(c(outer,inner)))
+
+  check_discrete(data,cols = unique(c(panel,by)))
 
   ans <- data_inventory_chunk(
     data = data,
-    outer = outer,
-    inner = inner,
+    by = by,
+    panel = panel,
     all_name = all_name,
     stacked = stacked,
     ...
   )
 
-  if(outer != ".total") {
+  if(by != ".total") {
     tot <- data_inventory_chunk(
-      data, outer = ".total", inner=".total", stacked = FALSE,
+      data, by = ".total", panel=".total", stacked = FALSE,
       all_name = all_name, ...
     )
     tot <- mutate(tot, .total = all_name)
     ans <- bind_rows(ans,tot)
   }
 
-  if(inner!=outer) {
-    ans <- mutate(ans, !!sym(inner) := replace_na(!!sym(inner),".total"))
-    ans <- fill(ans, !!sym(outer), .direction = "down")
+  if(panel != by) {
+    ans <- mutate(ans, !!sym(by) := replace_na(!!sym(by),".total"))
+    ans <- fill(ans, !!sym(panel), .direction = "down")
   }
 
-  if(inner==outer) {
-    ans <- mutate(ans, !!sym(outer) := replace_na(!!sym(outer),".total"))
+  if(panel == by) {
+    ans <- mutate(ans, !!sym(by) := replace_na(!!sym(by),".total"))
   }
 
   ans
@@ -178,8 +182,8 @@ data_inventory_data <- function(data, outer,inner=outer,all_name = "all",
 #' ans <- pt_data_study(data, study_col = "STUDYf")
 #'
 #' @export
-pt_data_study <- function(data, study_col = "STUDY", inner = study_col, ...) {
-  pt_data_inventory(data  = data, outer = study_col, inner = inner, ...)
+pt_data_study <- function(data, study_col = "STUDY", panel = study_col, ...) {
+  pt_data_inventory(data  = data, by = study_col, panel = panel, ...)
 }
 
 #' Create a data inventory table
@@ -189,8 +193,8 @@ pt_data_study <- function(data, study_col = "STUDY", inner = study_col, ...) {
 #'
 #' @inheritParams pt_cont_long
 #'
-#' @param outer the outer grouping variable; may be character or quosure
-#' @param inner the inner grouping variable; may be character or quosure
+#' @param by the outer grouping variable; may be character or quosure
+#' @param panel the panel grouping variable; may be character or quosure
 #' @param inner_summary if `TRUE`, then a summary of the inner variable will
 #' be provided
 #' @param drop_miss if `TRUE`, then `MISS` will be dropped, but only when all
@@ -210,19 +214,19 @@ pt_data_study <- function(data, study_col = "STUDY", inner = study_col, ...) {
 #' @examples
 #' data <- pmtables:::data("id")
 #'
-#' ans <- pt_data_inventory(data, outer = .cols("Renal function" = RFf))
+#' ans <- pt_data_inventory(data, by = .cols("Renal function" = RFf))
 #'
 #'
 #' @export
-pt_data_inventory <- function(data, outer = ".total", inner = outer,
+pt_data_inventory <- function(data, by = ".total", panel = by,
                               inner_summary = TRUE, drop_miss = FALSE,
                               stacked = FALSE, table = NULL,
                               all_name = "all", ...) {
 
-  outer <- new_names(outer,table)
-  inner <- new_names(inner,table)
+  by <- new_names(by,table)
+  panel <- new_names(panel,table)
 
-  if(inner==outer | stacked) {
+  if(panel==by | stacked) {
     inner_summary <- FALSE
   }
 
@@ -230,17 +234,17 @@ pt_data_inventory <- function(data, outer = ".total", inner = outer,
 
   ans <- data_inventory_data(
     data,
-    outer = outer,
-    inner = inner,
+    by = by,
+    panel = panel,
     stacked = stacked,
     all_name = all_name,
     ...
   )
 
-  if(exists(inner,ans)) {
+  if(exists(by,ans)) {
     ans <- mutate(
       ans,
-      !!sym(inner) := ifelse(!!sym(inner)==".total", total_name, !!sym(inner))
+      !!sym(by) := ifelse(!!sym(by)==".total", total_name, !!sym(by))
     )
   }
 
@@ -275,22 +279,22 @@ pt_data_inventory <- function(data, outer = ".total", inner = outer,
     }
   }
 
-  if(inner!=outer) {
+  if(panel != by) {
     ans <- mutate(
       ans,
       .total = NULL,
-      !!sym(outer) := paste0(names(outer), ": ", !!sym(outer))
+      !!sym(panel) := paste0(names(panel), ": ", !!sym(panel))
     )
-    ans <- rename(ans,inner)
-    ans <- rename(ans,outer)
-    out <- gt(ans,groupname_col=names(outer))
+    ans <- rename(ans,panel)
+    ans <- rename(ans,by)
+    out <- gt(ans,groupname_col=names(panel))
   }
-  if(inner==outer & outer != ".total") {
-    ans <- rename(ans, outer)
+  if(panel==by & by != ".total") {
+    ans <- rename(ans, by)
     ans <- mutate(ans,.total = NULL)
     out <- gt(ans)
   }
-  if(outer==".total" & inner==outer ) {
+  if(by==".total" & panel == by ) {
     ans <- mutate(ans,.total = NULL)
     out <- gt(ans)
   }
@@ -306,16 +310,17 @@ pt_data_inventory <- function(data, outer = ".total", inner = outer,
   out
 }
 
-data_inventory_stacked <- function(data,inner,stack_by,...) {
-  pt_data_inventory(
-    data,
-    outer = outer,
-    inner = inner,
-    inner_summary = FALSE,
-    stacked = TRUE,
-    ...
-  )
-}
+# data_inventory_stacked <- function(data,inner,stack_by,...) {
+#   pt_data_inventory(
+#     data,
+#     by = outer,
+#     inner = inner,
+#     inner_summary = FALSE,
+#     stacked = TRUE,
+#     ...
+#   )
+# }
+
 to_html <- function(x) {
   if(interactive()) return(x)
   as_raw_html(x)
