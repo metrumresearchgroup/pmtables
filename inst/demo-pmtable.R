d2e954b1896e870e88139d74b4d7616202093e05
Kyle Baron <kyleb@metrumrg.com>
2020-07-30 06:36:41 -0500

    fix pipe vignette to use st_sizes

--- inst/demo-pmtable.R
+++ inst/demo-pmtable.R
@@ -19,6 +19,7 @@ library(dplyr)
 library(pmtables)
 library(tidyverse)
 library(tidyselect)
+library(assertthat)
 library(yspec)
 
 #' # Setup
@@ -38,7 +39,8 @@ d <- filter(data, SEQ==1)
 
 #' ## Stacked by endpoint
 
-#+ results = 'asis'
+
+#+ pt-inventory-data-stacked, results = 'asis'
 x <- pt_data_inventory(
   data,
   by = vars(Study = "STUDYf"),
@@ -49,6 +51,41 @@ x <- pt_data_inventory(
   panel = rowpanel(c("Endpoint:" = "SEQf"), prefix_name = TRUE)
 )
 
+#+ include = FALSE
+out <- pt_data_inventory(
+  data,
+  by = vars(Study = "STUDYf"),
+  panel = vars("Endpoint" = "SEQf"),
+  stacked = TRUE
+)
+
+a <- group_by(data, SEQf,STUDYf)
+b <- summarize(
+  a,
+  NID = length(unique(SUBJ)),
+  NMISS = sum(is.na(DV) & BQL==0),
+  NBQL = sum(BQL !=0),
+  NOBS = sum(!is.na(DV)),
+  .groups = "drop"
+)
+dd <- group_by(b, SEQf) %>% mutate(TOBS = sum(NOBS), TBQL = sum(NBQL)) %>% ungroup()
+e <- mutate(dd, POBS = 100*NOBS/TOBS, PBQL = 100*NBQL/TOBS)
+f <- mutate(e, across(c(POBS,PBQL), .fns = ~ifelse(is.nan(.x), 0, .x)))
+g <- f
+i <- map_df(split(g, g$SEQf, drop=TRUE), function(df) {
+  df2 <- select_if(df, is.numeric)
+  bind_rows(df,summarize(df2,across(everything(), sum)))
+})
+j <- mutate(i, across(POBS:PBQL, pmtables:::digit1))
+
+assert_that(all(out$data$Number.SUBJ==j$NID))
+assert_that(all(out$data$Number.OBS==j$NOBS))
+assert_that(all(out$data$Number.BQL==j$NBQL))
+assert_that(all(out$data$Number.MISS==j$NMISS))
+assert_that(all(out$data$Percent.BQL==j$PBQL))
+assert_that(all(out$data$Percent.OBS==j$POBS))
+
+#+
 
 #' \clearpage
 
@@ -63,7 +100,52 @@ pt_data_inventory(
   panel = vars(Race = ASIANf)
 ) %>% as_stable(wrapw = TRUE, r_file = "test.R", output_file = "test.tex")
 
+#+
+out <- pt_data_inventory(
+  d,
+  by = vars(Study = "STUDYf"),
+  panel = vars(Race = ASIANf)
+) %>% as_stable(inspect = TRUE) %>% get_stable_data()
 
+a <- group_by(d, ASIANf,STUDYf)
+b <- summarize(
+  a,
+  NID = length(unique(SUBJ)),
+  NMISS = sum(is.na(DV) & BQL==0),
+  NBQL = sum(BQL !=0),
+  NOBS = sum(!is.na(DV)),
+  .groups = "drop"
+)
+dd <- b %>% mutate(TOBS = sum(NOBS), TBQL = sum(NBQL)) %>% ungroup()
+ddd <- group_by(dd, ASIANf) %>% mutate(GOBS = sum(NOBS), GBQL = sum(NBQL)) %>% ungroup()
+e <- mutate(
+  ddd,
+  POBS = 100*NOBS/TOBS,
+  PBQL = 100*NBQL/TOBS,
+  GPOBS = 100*NOBS/GOBS,
+  GPBQL = 100*NBQL/GBQL
+)
+f <- mutate(e, across(c(POBS,PBQL), .fns = ~ifelse(is.nan(.x), 0, .x)))
+
+g <- summarise_at(f, -c(1,2), sum)
+h <- bind_rows(f,g)
+i <- mutate(h, across(POBS:PBQL, pmtables:::digit1))
+
+
+check <- list(
+  c("NID", "Number.SUBJ"),
+  c("NMISS", "Number.MISS"),
+  c("NOBS", "Number.OBS"),
+  c("NBQL", "Number.BQL"),
+  c("POBS", "Overall percent.OBS"),
+  c("PBQL", "Overall percent.BQL")
+)
+
+ans <- map_lgl(check, ~ identical(i[[.x[1]]], out$data[[.x[2]]]))
+assert_that(all(ans))
+
+
+#+
 
 #' \clearpage
 
@@ -76,6 +158,48 @@ pt_data_inventory(
   by = vars(Study = "STUDYf")
 ) %>% as_stable(wrapw = TRUE, r_file = "test.R", output_file = "test.tex")
 
+#+ include = FALSE
+out <- pt_data_inventory(
+  d,
+  by = vars(Study = "STUDYf")
+) %>% as_stable(inspect = TRUE) %>% get_stable_data()
+
+a <- group_by(d,STUDYf)
+b <- summarize(
+  a,
+  NID = length(unique(SUBJ)),
+  NMISS = sum(is.na(DV) & BQL==0),
+  NBQL = sum(BQL !=0),
+  NOBS = sum(!is.na(DV)),
+  .groups = "drop"
+)
+dd <- b %>% mutate(TOBS = sum(NOBS), TBQL = sum(NBQL)) %>% ungroup()
+e <- mutate(
+  dd,
+  POBS = 100*NOBS/TOBS,
+  PBQL = 100*NBQL/TOBS
+)
+f <- mutate(e, across(c(POBS,PBQL), .fns = ~ifelse(is.nan(.x), 0, .x)))
+
+g <- summarise_at(f, -c(1), sum)
+h <- bind_rows(f,g)
+i <- mutate(h, across(POBS:PBQL, pmtables:::digit1))
+
+
+check <- list(
+  c("NID", "Number.SUBJ"),
+  c("NMISS", "Number.MISS"),
+  c("NOBS", "Number.OBS"),
+  c("NBQL", "Number.BQL"),
+  c("POBS", "Percent.OBS"),
+  c("PBQL", "Percent.BQL")
+)
+
+ans <- map_lgl(check, ~ identical(i[[.x[1]]], out$data[[.x[2]]]))
+assert_that(all(ans))
+
+
+#+
 
 #' \clearpage
 
@@ -93,17 +217,63 @@ pt_cat_wide(
   cols = vars(Formulation = FORMf,Sex = SEXf,"Race group" = ASIANf)) %>%
   as_stable(wrapw = TRUE, r_file = "test.R", output_file = "test.tex")
 
+#+ include = FALSE
+out <- pt_cat_wide(
+  data = data,
+  cols = vars(Formulation = FORMf,Sex = SEXf,"Race group" = ASIANf)
+) %>% as_stable(inspect = TRUE) %>% get_stable_data()
+
+w <- pivot_longer(data, cols = c("FORMf", "SEXf", "ASIANf"))
+N <- length(unique(data$ID))
+x <- group_by(w,name,value) %>% summarise(
+  number = length(unique(ID)),
+  percent = pmtables:::digit1(100*number/N),
+  .groups = "drop"
+)
+y <- mutate(
+  x,
+  name = paste0(name, '.', value),
+  value = paste0(number, " (", percent, ")"),
+  number = NULL, percent=NULL,
+  name = gsub("-", "", name, fixed = TRUE)
+)
+ans <- y$value
+names(ans) <- y$name
+out$list <- as.list(out$data)
+check <- list(
+  c("ASIANf.Asian", "Race group.Asian"),
+  c("ASIANf.nonAsian", "Race group.non-Asian"),
+  c("FORMf.tablet", "Formulation.tablet"),
+  c("FORMf.capsule", "Formulation.capsule"),
+  c("FORMf.troche", "Formulation.troche"),
+  c("SEXf.male", "Sex.female"),
+  c("SEXf.female","Sex.male" )
+)
+comp <- map_df(check, ~ tibble(ans = ans[[.x[1]]], out =  out$list[[.x[2]]]))
+identical(comp$ans, comp$out)
+
+#+
+
 ##' ## Paneled (limited utility, IMO)
 
 #+ results = 'asis'
 
-
-pt_cat_wide(
+out <- pt_cat_wide(
   data = data,
   cols = vars(Formulation = FORMf,Sex = SEXf,"Race group" = ASIANf),
   panel = c(Study = "STUDYf")) %>%
   as_stable(wrapw = TRUE, r_file = "test.R", output_file = "test.tex")
 
+
+#+ include = FALSE
+out <- pt_cat_wide(
+  data = data,
+  cols = vars(Formulation = FORMf,Sex = SEXf,"Race group" = ASIANf),
+  panel = c(Study = "STUDYf")) %>%
+  as_stable(inspect=TRUE) %>% get_stable_data()
+as.data.frame(out$data)
+
+#+
 
 #' \clearpage
 
