ba01b76cf3587e6d0c2965881c632de4981e855f
Kyle Baron <kyleb@metrumrg.com>
2020-08-11 16:06:53 -0500

    fix test for font size

--- tests/testthat/test-sizes.R
+++ tests/testthat/test-sizes.R
@@ -9,6 +9,6 @@ context("test-sizes")
 
 test_that("test-sizes-fontsize", {
   out <- inspect(ptdata(), sizes = tab_size(font = "tiny"))
-  expect_match(out$output[1], "\\tiny")
+  expect_match(out$output[1], "\\tiny", fixed = TRUE)
 })
 
