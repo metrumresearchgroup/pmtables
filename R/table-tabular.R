d4f810a4f5fb41782f6befb4a0ceee396d5b0402
Kyle Baron <kyleb@metrumrg.com>
2021-01-26 07:20:01 -0600

    coerce to data frame before making tabular

--- R/table-tabular.R
+++ R/table-tabular.R
@@ -71,6 +71,8 @@ make_tabular <- function(data, prime_fun = tab_prime,
 
   data <- prime_fun(data, escape_fun)
 
+  data <- as.data.frame(data)
+
   tab <- modify(data, function(x) {
     formatC(x, width = max(nchar(x)))
   })
