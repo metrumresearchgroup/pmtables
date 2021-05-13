a41062420ad56055a90872a73f85275595c1476b
Kyle Barrett <barrettk@metrumrg.com>
2021-05-13 12:53:55 -0400

    unit argument and documentation for pt_demographics

--- R/demographics-table.R
+++ R/demographics-table.R
@@ -11,16 +11,32 @@ smr <- function(value = seq(1,5)) {
 #'
 #' This function makes a single table from both continuous and categorical data.
 #'
-#' @param data
-#' @param cols_cont
-#' @param cols_cat
+#' @param data 	the data frame to summarize; the user should filter or subset so that data contains exactly the records to be summarized; pmtables will not add or remove rows prior to summarizing data
+#' @param cols_cont the continuous columns to summarize; may be character vector or quosure
+#' @param cols_cat the categorical columns to summarize; may be character vector or quosure
+#' @param span variable name for column spanner
+#' @param stat_name name of statistic column
+#' @param units optional units for each summarized column; must be a named list
+#' @param stat_width distance (in cm) between the statistic and summarized columns
 #'
 #' @return
 #' An object of class `pmtable`
 #'
 #' @examples
+#'out <- pt_demographics(
+#' data = pmt_first,
+#' cols_cont = c('Age (yrs)'='AGE', 'Weight (kg)'='WT'),
+#' cols_cat = c(Sex='SEXf',Race='ASIANf'),
+#' span = c("Study"="STUDYf"),
+#' stat_name = "Statistic"
+#')
+#'
+#' @details
+#' The data is summarized using [pt_cont_long()] and [pt_cat_long()] for the continuous and categorical data respectively.
 #'
 #' @seealso [pt_cont_long()], [pt_cat_long()]
+#'
+#' @return An object with class pmtable; see class-pmtable.
 #'
 #' @export
 pt_demographics <- function(data, cols_cont, cols_cat, span, units = NULL,
@@ -34,6 +50,7 @@ pt_demographics <- function(data, cols_cont, cols_cat, span, units = NULL,
   span <- new_names(span)
   span_lvl <- levels(fct_inorder(data[[unname(span)]]))
 
+  ### Continuous Variables ###
   cont_table <- pt_cont_long(
     data,
     cols = cols_cont,
@@ -51,8 +68,7 @@ pt_demographics <- function(data, cols_cont, cols_cat, span, units = NULL,
   cont_table <- dplyr::select(cont_table,-c(`All data`))
   cont_table <- dplyr::rename(cont_table, name=Variable, level=name)
 
-  # add units
-
+  ### Categorical Variables ###
   cat_table <- pt_cat_long(
     data,
     cols = all_of(cols_cat),
@@ -60,8 +76,28 @@ pt_demographics <- function(data, cols_cont, cols_cat, span, units = NULL,
     summarize = "top"
   )
 
+  ### Combined Table ###
   table_data <- bind_rows(cont_table, cat_table[["data"]])
   table_data <- rename(table_data, !!sym(stat_name) := level)
+
+  # add units
+  units <- validate_units(units,data)
+  if(!is.null(units)){
+    all_cols <- as.list(c(cols_cont,cols_cat))
+    units <- units[match(names(units),all_cols)]
+    names(units) <- names(all_cols[match(names(units),all_cols)])
+
+    unit_names <- lapply(seq(units), function(i){
+      ifelse(units[[i]]!="",paste0(names(units[i]), " (",units[[i]],")"),names(units[i]))
+    })
+    names(unit_names) <- names(units)
+
+    for(i in 1:length(unit_names)){
+      table_data <- mutate(table_data, name = ifelse(name==names(unit_names[i]), unit_names[[i]], name))
+    }
+  }
+
+
 
   align <- cols_left()
   ragged <- list(col_ragged(stat_width))
@@ -78,3 +114,6 @@ pt_demographics <- function(data, cols_cont, cols_cat, span, units = NULL,
 
   structure(ans, class = c("pmtable", class(ans)))
 }
+
+#' @rdname pt_demographics
+#'
--- tests/testthat/test-demographics-table.R
+++ tests/testthat/test-demographics-table.R
@@ -4,15 +4,17 @@ test_that("demographics data summary - simple", {
 
   out <- pt_demographics(
     data = pmt_first,
-    cols_cont = c('Age (yrs)'='AGE', 'Weight (kg)'='WT'),
+    cols_cont = c('Age'='AGE', 'Weight'='WT'),
     cols_cat = c(Sex='SEXf',Race='ASIANf'),
     span = c("Study"="STUDYf"),
+    units = list(WT="kg", AGE="yrs"),
     stat_name = "Statistic"
   )
+  #out %>% stable() %>% st_preview()
 
   expect_true(all(class(out)==c("pmtable", "list")))
 
   expect_equal(unique(out$data$name), c("Age (yrs)", "Weight (kg)", "Sex", "Race"))
-  #out %>% stable() %>% st_preview()
+
 
 })
