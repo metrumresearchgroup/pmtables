9803b04d2e6ab5c20ac301592cb1fb4829085dd1
Kyle Baron <kyleb@metrumrg.com>
2022-03-22 10:44:45 -0500

    refine cat table

--- R/table-cat-cat.R
+++ R/table-cat-cat.R
@@ -1,40 +1,134 @@
 #
 # across <- c(Formulation =  "FORMf", Sex = "SEXf")
 # down <- c(Renal = "RFf", `Race` = "ASIANf", Study = "STUDYf")
-# nn_factor <- function(x, col) {
-#   factor(x, levels = col, labels = names(col))
-# }
+nn_factor <- function(x, col) {
+  factor(x, levels = col, labels = names(col))
+}
+
+n_pct <- function(count, total) {
+  pct <- digit1(100*count/total)
+  as.character(glue("{count} ({pct})"))
+}
+
+#' Categorical by categorical table
+#'
+#' @export
+pt_cat_cat <- function(data, cols_down, cols_across) {
+  tab <- pt_cat_cat_data(data, cols_down, cols_across)
+  pt_cat_cat_style(tab)
+}
+#' @rdname pt_cat_cat
+#' @export
+pt_cat_cat_style <- function(tab) {
+    #final %>% st_new() %>%
+    #st_panel("name") %>%
+    #st_hline(at = 2, n = 2) %>%
+    #st_span_split(sep = '__') %>%
+    #st_blank("value") %>%
+    sub_name <- grepl("Summary", tab$value[1])
+    if(sub_name) {
+      #tab$name[1] <- NA_character_
+      #tab$value[1] <- NA_character_
+    }
+
+    object <- list(
+      data = tab,
+      panel = as.panel("name"),
+      #hline_at = c(2,2),
+      span_split = colsplit(sep = "__"),
+      cols_blank = c("value"),
+      sumrows = sumrow(nrow(tab), depanel =FALSE)
+    )
+    class(object) <- c("pmtable", class(object))
+    object
+}
+
+#' @rdname pt_cat_cat
+#' @export
+pt_cat_cat_data <- function(data, cols_down, cols_across) {
+  na_value <- "0 (0.00)"
+  all_cols <- unname(c(across, down))
+  check_discrete(data, cols_across)
+  check_discrete(data, cols_down)
+  across <- rename_cols(cols_across)
+  down <- rename_cols(cols_down)
+  NNN <- nrow(data) # The number of records
+
+  long <- pivot_longer(data, cols = all_of(unname(down)))
+  long2 <- pivot_longer(
+    long,
+    cols = all_of(unname(across)),
+    names_to = "name2", values_to = "value2"
+  )
+  summ <- group_by(long2, name, name2, value, value2)
+  summ <- count(summ)
+  summ <- ungroup(summ)
+  summ <- mutate(summ, name = nn_factor(name, down))
+  summ <- mutate(summ, name2 = nn_factor(name2, across))
+  summ <- mutate(summ, name2 = paste(name2, value2, sep = '__'))
+  summ$value2 <- NULL
+
+  # primary summary --------
+  summ1 <- mutate(summ, summary = n_pct(n, NNN))
+  summ1 <- pivot_wider(
+    select(summ1, -n),
+    names_from = "name2",
+    values_from = "summary"
+  )
+  ncol <- ncol(summ1)
+  summ1 <- mutate(summ1, across(seq(3, ncol), as.character))
+  summ1 <- mutate(summ1, across(seq(3, ncol), replace_na, na_value))
+
+  # summary of of down columns -------
+  summ2 <- distinct(summ, name, value, n)
+  summ2 <- group_by(summ2, name, value)
+  summ2 <- summarise(summ2, n = sum(n))
+  summ2 <- group_by(summ2, name)
+  summ2 <- mutate(summ2, Summary = n_pct(n, sum(n)))
+  summ2 <- ungroup(summ2)
+  summ2$n <- NULL
+  summ2 <- mutate(summ2, Summary = replace_na(Summary, na_value))
+
+  # column summary -------
+  # Count the across covariates
+  summ3 <- pivot_longer(data, cols = all_of(unname(across)))
+  summ3 <- group_by(summ3, name, value)
+  summ3 <- count(summ3)
+  summ3 <- ungroup(summ3)
+  # Substitutes rename
+  summ3 <- mutate(summ3, name = nn_factor(name, across))
+  summ3 <- mutate(summ3, name = paste0(name, "__", value))
+  summ3$value <- NULL
+  summ3 <- mutate(summ3, Summary = n_pct(n, NNN))
+  summ3 <- mutate(summ3, Summary = replace_na(Summary, na_value))
+  summ3 <- pivot_wider(select(summ3, -n), values_from = "Summary")
+  summ3 <- mutate(summ3, Summary = paste0(NNN, " (100.0)"))
+  summ3$name <- "All data"
+  summ3$value <- "Summary"
+  # end column summary ------
+
+  tab <- left_join(summ1, summ2, by = c("name", "value"))
+  tab <- select(tab, name, value, everything())
+
+  summ3 <- select(summ3, names(tab))
+  tabl <- slice(tab, nrow(tab))
+  for(j in seq(2,ncol(tab))) {
+    tabl[j] <- ""
+  }
+  tabl[2] <- "\\hline"
+  tab <- bind_rows(tab, tabl, summ3)
+  #tab <- select(tab, name, value, Summary, everything())
+  #names(tab) <- sub("Summary", tex_bold("Summary"), names(tab), fixed = TRUE)
+  tab
+}
+
+#
+
 #
 #
-# data <- select(data, all_of(unname(c(across, down, "ID"))))
-# data <- mutate(data, NNN = n())
-# NNN <- data$NNN[1]
+
 #
-# long <- pivot_longer(data, cols = all_of(unname(down)))
-# long2 <- pivot_longer(long, cols = all_of(unname(across)),
-#                       names_to="name2", values_to="value2")
-#
-# summ <- group_by(long2, name, name2, value, value2)
-# summ <- count(summ)
-# summ <- ungroup(summ)
-#
-# summ <- mutate(summ, name = nn_factor(name, down))
-# summ <- mutate(summ, name2 = nn_factor(name2, across))
-#
-# summ <- mutate(summ, name2 = paste(name2, value2, sep = '__'))
-#
-#
-# summ$value2 <- NULL
-#
-# summ1 <- mutate(
-#   summ,
-#   summary = paste0(n, " (",  pmtables:::digit1(100*n/NNN), ")")
-# )
-# summ1 <- pivot_wider(
-#   select(summ1, -n),
-#   names_from="name2",
-#   values_from="summary"
-# )
+
 #
 # summ2 <- distinct(
 #   summ,
@@ -51,21 +145,7 @@
 # summ1 <- select(summ1, name, value, everything())
 #
 #
-# bot <- pivot_longer(data, cols = all_of(unname(across)))
-# bot <- group_by(bot, name, value, NNN)
-# bot <- count(bot)
-# bot <- ungroup(bot)
-# bot <- mutate(bot, name = nn_factor(name, across))
-# bot <- mutate(bot, name = paste0(name, "__", value))
-# bot$value <- NULL
-# bot <- mutate(bot, Summary = paste0(n, " (", pmtables:::digit1(100*n/NNN), ")"))
-#
-# bot <- pivot_wider(select(bot, -n), values_from = "Summary")
-#
-# bot <- mutate(bot, Summary = paste0(NNN, " (100.0)"))
-#
-# bot$name <- NA
-# bot$value <- tex_bold("Summary")
+
 #
 # final <- summ1 %>% mutate(NNN= NULL)
 # bot <- select(bot, names(final))
