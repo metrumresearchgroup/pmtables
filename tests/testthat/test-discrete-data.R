355cedbbf886a725f03126f7549ff971a727a184
Kyle Baron <kyleb@metrumrg.com>
2022-03-29 14:04:53 -0500

    add tests defining denom behavior

--- tests/testthat/test-discrete-data.R
+++ tests/testthat/test-discrete-data.R
@@ -1,3 +1,5 @@
+library(testthat)
+library(pmtables)
 
 context("test-cat-data")
 
@@ -62,4 +64,53 @@ test_that("discrete data summary wide - by", {
   expect_equal(nrow(ans),length(unique(data[["STUDY"]])))
   # add 1 for summary, 1 for N
   expect_equal(ncol(ans),l + 1 + 1)
+})
+
+extract_pct <- function(x) {
+  x <- strsplit(x, "\\(|\\)")
+  x <- sapply(x, "[[", 2)
+  as.numeric(x)
+}
+
+test_that("discrete data summary - with group denominator", {
+  data <- pmt_first
+  cols <- c("FORMf")
+  # The long summary sums by column
+  ans <- cat_data(data, cols = cols, wide = FALSE, by = "STUDYf", denom = "group")
+  expect_is(ans,"data.frame")
+  ans <- ans[,seq(3, ncol(ans))]
+  ans[] <- lapply(ans, extract_pct)
+  expect_true(all(colSums(ans)==100))
+  # The wide summary sums by row
+  ans <- cat_data(data, cols = cols, wide = TRUE, by = "STUDYf", denom = "group")
+  expect_is(ans,"data.frame")
+  ans <- ans[,seq(3, ncol(ans))]
+  ans[] <- lapply(ans, extract_pct)
+  expect_true(all(rowSums(ans)==100))
+})
+
+test_that("discrete data summary - with total denominator", {
+  data <- pmt_first
+  cols <- c("FORMf")
+  # The long table summs to 100% across everything
+  ans <- cat_data(
+    data, cols = cols, wide = FALSE, by = "STUDYf", denom = "total",
+    panel = "ASIANf"
+  )
+  expect_is(ans,"data.frame")
+  ans <- ans[, seq(4, ncol(ans))]
+  ans[] <- lapply(ans, extract_pct)
+  summ <- sum(ans)
+  expect_true(summ > 99.5 & summ < 100.2)
+  # The wide table summs to 100% within column group
+  ans <- cat_data(
+    data,
+    cols = c(cols, "ASIANf"), panel = "SEXf",
+    wide = TRUE, by = "STUDYf", denom = "total",
+  )
+  expect_is(ans,"data.frame")
+  ans <- ans[,grepl("ASIAN", names(ans))]
+  ans[] <- lapply(ans, extract_pct)
+  summ <- sum(ans)
+  expect_true(summ > 99.5 & summ < 100.2)
 })
